fec45ef001b7c6d1fa49f0a9904b087af02d9ae9
Adrian Vladu <avladu@cloudbasesolutions.com>
2022-05-02 14:21:14 +0300

    Use upper constraints from stable/victoria

--- BuildAutomation/BuildCloudbaseInitSetup.ps1
+++ BuildAutomation/BuildCloudbaseInitSetup.ps1
@@ -97,7 +97,7 @@ try
     ExecRetry { PipInstall "pip" -update $true }
     ExecRetry { PipInstall "wheel" -update $true }
 
-    ExecRetry { PullInstall "requirements" "https://github.com/openstack/requirements" }
+    ExecRetry { PullInstall "requirements" "https://github.com/openstack/requirements" "stable/victoria"}
     $upper_constraints_file = $(Resolve-Path ".\requirements\upper-constraints.txt").Path
     $env:PIP_CONSTRAINT = $upper_constraints_file
     $env:PIP_NO_BINARIES = "cloudbase-init"
